8983b11019bc60331938dafb0429a93235a7e9f7
Hyo <kyh12180@gmail.com>
2023-07-28 09:36:33 +0900

    project 모델 생성, 기존 테이블 일부 수정

--- baro_test/baro_test/settings.py
+++ baro_test/baro_test/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'comments',
     'posts',
     'follows',
+    'projects',
 
 ]
 
--- baro_test/posts/models.py
+++ baro_test/posts/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from account.models import User
 from django.utils import timezone
+from projects.models import Project
 
 # Create your models here.
 class Post(models.Model):
     # TODO post_id는 P로 시작해야함! 로직 수정 때 확인
     post_id = models.CharField(primary_key=True, max_length=10)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='post')
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, related_name='post')
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True, null=True)
     post_time = models.DateTimeField(auto_now_add=True)
