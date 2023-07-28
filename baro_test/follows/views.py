a5242ded9d15564f16a490ec2af2720b8e497d62
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2023-07-28 10:31:31 +0900

    following

--- baro_test/follows/views.py
+++ baro_test/follows/views.py
@@ -36,4 +36,36 @@ class SubscriptionListView(ListView):
         context = super().get_context_data(**kwargs)
         uploader_ids=self.get_queryset()
         context["user_list"] = User.objects.filter(pk__in=uploader_ids)
+        context["image_post_list"] = ImagePost.objects.filter(user__in=uploader_ids)
+        return context
+    
+@method_decorator(login_required,'get')
+class FollowView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('account:detail',kwargs={'pk':self.request.GET.get('account_pk')})
+    def get(self,request,*args,**kwargs):
+        uploader=get_object_or_404(User,pk=self.request.GET.get('account_pk'))
+        user=self.request.user
+
+        subscription = FollowUploader.objects.filter(user=user,uploader=uploader)
+        if subscription.exists():
+            subscription.delete()
+        else:
+            FollowUploader(user=user,uploader=uploader).save()
+        return super(FollowView,self).get(request,*args, **kwargs)
+
+class FollowingListView(ListView):
+    model=User
+    context_object_name='user_list'
+    template_name="follows/list.html"
+    paginate_by=5
+
+    def get_queryset(self):
+        writes=FollowUploader.objects.filter(user=self.request.user).values_list('uploader')
+        return writes
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        uploader_ids=self.get_queryset()
+        context["user_list"] = User.objects.filter(pk__in=uploader_ids)
+        context["image_post_list"] = ImagePost.objects.filter(user__in=uploader_ids)
         return context
