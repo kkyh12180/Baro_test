6b7aab7fd770fe493ca50bac65bb34d868e13b8f
Hyo <kyh12180@gmail.com>
2023-07-31 14:28:24 +0900

    댓글 좋아요 기능 추가

--- baro_test/comments/urls.py
+++ baro_test/comments/urls.py
@@ -8,4 +8,6 @@ app_name ="comment"
 urlpatterns = [
     path('create/<str:pk>',CommentCreateView.as_view(),name='create'),
     path('delete/<str:pk>',CommentDeleteView.as_view(),name='delete'),
+    path('image_comment_like/', CommentImagePostLikeView.as_view(), name='image_comment_like'),
+
 ]
