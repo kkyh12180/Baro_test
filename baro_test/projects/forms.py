fd186f8a8fda7c2c98416b7921d9cc679992e0db
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2023-07-31 10:03:11 +0900

    project images

--- baro_test/projects/forms.py
+++ baro_test/projects/forms.py
@@ -1,7 +1,27 @@
 from django.forms import ModelForm
 from projects.models import Project
+from django import forms
+import base64
 
 class ProjectCreationForm(ModelForm):
     class Meta:
         model=Project
-        fields = ['title','description']
+        fields = ['title','description','profile']
+
+    profile = forms.ImageField(required=False)
+
+    def clean_profile(self):
+        image = self.cleaned_data.get('profile')
+        if image :
+            content = image.read()
+            image_binary = base64.b64encode(content).decode('UTF-8')
+            self.cleaned_data['profile_image'] = image_binary
+        return image
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.profile_image = self.cleaned_data.get('profile_image', '')
+        if commit:
+            instance.save()
+        return instance
+    
