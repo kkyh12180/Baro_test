af7d3b1fd5c46ccc771c46a94c117fc02791ffee
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2023-08-01 15:22:08 +0900

    search main

--- baro_test/search/views.py
+++ baro_test/search/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from search.search_in_elastic import query_to_elasticsearch
 
 # Create your views here.
-def hello_world(request):
-    return render(request,'base.html')
+def main(request):
+    return render(request,'search/main.html')
+
+def test(request):
+    data_list = query_to_elasticsearch()
+    context = {'data_list': data_list}
+    return render(request,'search/test.html',context)
