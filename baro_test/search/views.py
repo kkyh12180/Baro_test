8afa40ec4c92677a164060ceace33089b6c7db3a
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2023-08-01 15:40:26 +0900

    seach tool

--- baro_test/search/views.py
+++ baro_test/search/views.py
@@ -4,6 +4,14 @@ from search.search_in_elastic import query_to_elasticsearch
 
 # Create your views here.
 def main(request):
+    if request.method == 'POST':
+        prompt=request.POST.get('prompt','')
+        negative_prompt = request.POST.get('negative_prompt','')
+        context={
+            'prompt':prompt,
+            'negative_prompt':negative_prompt,
+        }
+        return render(request,'search/result.html',context)
     return render(request,'search/main.html')
 
 def test(request):
