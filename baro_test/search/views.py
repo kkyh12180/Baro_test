2962de00a30fdc14f916beaa2dc8fc07ab9c68d1
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2023-08-02 11:27:57 +0900

    test => result

--- baro_test/search/views.py
+++ baro_test/search/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from search.search_in_elastic import *
+from search.logs import *
 
 # Create your views here.
 def main(request):
@@ -8,14 +9,12 @@ def main(request):
         prompt=request.POST.get('prompt','')
         negative_prompt = request.POST.get('negative_prompt','')
         context=result(prompt=prompt,negative_prompt=negative_prompt)
-        return render(request,'search/test.html',context)
+        return render(request,'search/result.html',context)
     return render(request,'search/main.html')
 
 def test(request):
-    query_maker=QueryMake()
-    data_list = query_maker.query_to_elastic()
-    context = {'data_list': data_list}
-    return render(request,'search/test.html',context)
+    logs()
+    return render(request,'search/main.html')
 
 def result(prompt,negative_prompt):
     query_maker=QueryMake()
