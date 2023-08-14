15be67ae6dad7b71c83b62d4824446cf891562f0
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2023-08-14 14:00:18 +0900

    image username

--- images/Clear_EXIF.py
+++ images/Clear_EXIF.py
@@ -1,4 +1,3 @@
-import base64
 from PIL import Image
 from PIL.ExifTags import TAGS
 
--- projects/forms.py
+++ projects/forms.py
@@ -1,7 +1,6 @@
 from django.forms import ModelForm
 from projects.models import Project
 from django import forms
-import base64
 
 class ProjectCreationForm(ModelForm):
     class Meta:
