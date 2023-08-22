dcd4482c945322a4cbd0a653bab20f5d10dd768a
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2023-08-22 14:50:32 +0900

    언어

--- images/urls.py
+++ images/urls.py
@@ -1,14 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
 from images.views import *
 
 # app_name 명시
-# 추후 accountapp:hello_world 로 접근이 가능해지기 때문
 app_name = 'images'
 
 urlpatterns = [
-    # views.py의 hello_world 불러옴
-    # path('mypage/<str:pk>', Mypage.as_view(), name='hello_world'),
     path('', ImagePostListView.as_view(), name='list'),
     path('create/', ImagePostCreateView.as_view(), name='create'),
     path('detail/<str:pk>', ImagePostDetailView.as_view(), name='detail'),
