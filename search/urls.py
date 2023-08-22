e56d62138a26c09d1a905cc80037226331d92eb2
Hyo <kyh12180@gmail.com>
2023-08-22 09:20:40 +0900

    chatgpt 주석 처리

--- search/urls.py
+++ search/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('delete/',delete_all,name='delete_all'),
     path('delete/<str:pk>',delete,name='delete'),
     path('result/',ResultView.as_view(), name="result"),
-    path('chat/',chat_view,name="chat_view"),
+    # path('chat/',chat_view,name="chat_view"),
     path('rank/',rank,name="rank"),
 ]
