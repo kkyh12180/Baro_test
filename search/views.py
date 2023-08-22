ed6a1f482ac87cd16bcc9a6aa95c6eba5659f434
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2023-08-22 13:39:25 +0900

    search 주석

--- search/views.py
+++ search/views.py
@@ -1,69 +1,31 @@
-from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.views.generic import ListView, View
+from django.views.generic import ListView
 from django.utils import timezone
-from django.core.paginator import Paginator
 
+#from search.prompt_make import MakeImage
 from search.search_in_elastic import *
-from search.models import *
 from search.elastic import *
-from search.pocket import pocket
-from search.prompt_make import MakeImage
+from search.models import *
 from images.models import *
 
 import string
 import random
-# import openai
 
-# openai.api_key = pocket().chat_key
-
-class SearchListView(ListView):
+#유저의 검색했던 로그를 15개 보여주기
+class LogListView(ListView):
     template_name = 'search/main.html'
     context_object_name = 'log_list'
-    paginate_by = 10
 
+    # 유저의 검색 로그를 시간 순으로 15개 가져오기
     def get_queryset(self):
         user_pk = self.request.user.pk
         return Prompt_log.objects.filter(user_id=user_pk).order_by('-created_at')[:15]
 
     def post(self, request, *args, **kwargs):
-        user_pk = request.user.pk
-        prompt = request.POST.get('prompt', '')
-        negative_prompt = request.POST.get('negative_prompt', '')
-        prompt, negative_prompt = tokenizer(prompt, negative_prompt)
+        return result_page(request)
 
-        if user_pk:
-            prompt_log_check = Prompt_log.objects.filter(user_id=user_pk, prompt=prompt, negative_prompt=negative_prompt)
-
-            if not prompt_log_check:
-                plid = ""
-                while True:
-                    letters_set = string.ascii_letters
-                    num = random.randrange(1, 10)
-                    random_list = random.sample(letters_set, num)
-                    random_str = f"L{''.join(random_list)}"
-                    try:
-                        Prompt_log.objects.get(prompt_log_id=random_str)
-                    except Prompt_log.DoesNotExist:
-                        plid = random_str
-                        break
-
-                Prompt_log.objects.create(prompt_log_id=plid, user_id=user_pk, prompt=prompt, negative_prompt=negative_prompt)
-            else:
-                prompt_log_check[0].created_at = timezone.now()
-                prompt_log_check[0].save()
-        
-        # Store the search results in the session
-        request.session['search_query'] = {'prompt': prompt, 'negative_prompt': negative_prompt}
-        
-        # Build the URL for redirecting with pagination
-        redirect_url = reverse('search:result')
-        redirect_url += f'?page=1'
-        
-        # Redirect to ResultView with pagination
-        return redirect(redirect_url)
-
+#검색 결과가 나오는 창
 class ResultView(ListView):
     template_name = 'search/result.html'
     context_object_name = 'data_list'
@@ -80,43 +42,52 @@ class ResultView(ListView):
         data_list = query_maker.query_to_elastic(prompt, negative_prompt)
         return data_list
     
+    #검색 결과 창에서 재검색을 실행시 코드
     def post(self, request, *args, **kwargs):
-        user_pk = request.user.pk
-        prompt = request.POST.get('prompt', '')
-        negative_prompt = request.POST.get('negative_prompt', '')
-        prompt, negative_prompt = tokenizer(prompt, negative_prompt)
+        return result_page(request)
 
-        if user_pk:
-            prompt_log_check = Prompt_log.objects.filter(user_id=user_pk, prompt=prompt, negative_prompt=negative_prompt)
+#검색 창에서 검색 실행
+def result_page(request):
+    user_pk = request.user.pk
+    prompt = request.POST.get('prompt', '')
+    negative_prompt = request.POST.get('negative_prompt', '')
+    #검색 결과를 토큰화
+    prompt, negative_prompt = tokenizer(prompt, negative_prompt)
 
-            if not prompt_log_check:
-                plid = ""
-                while True:
-                    letters_set = string.ascii_letters
-                    num = random.randrange(1, 9)
-                    random_list = random.sample(letters_set, num)
-                    random_str = f"PL{''.join(random_list)}"
-                    try:
-                        Prompt_log.objects.get(prompt_log_id=random_str)
-                    except Prompt_log.DoesNotExist:
-                        plid = random_str
-                        break
+    #user가 존재한다면 검색로그에 추가
+    if user_pk:
+        prompt_log_check = Prompt_log.objects.filter(user_id=user_pk, prompt=prompt, negative_prompt=negative_prompt)
 
-                Prompt_log.objects.create(prompt_log_id=plid, user_id=user_pk, prompt=prompt, negative_prompt=negative_prompt)
-            else:
-                prompt_log_check[0].created_at = timezone.now()
-                prompt_log_check[0].save()
-        
-        # Store the search results in the session
-        request.session['search_query'] = {'prompt': prompt, 'negative_prompt': negative_prompt}
-        
-        # Build the URL for redirecting with pagination
-        redirect_url = reverse('search:result')
-        redirect_url += f'?page=1'
-        
-        # Redirect to ResultView with pagination
-        return redirect(redirect_url)
+        #해당 prompt를 처음 검색하면 새로운 Promp_log를 생성
+        if not prompt_log_check:
+            plid = ""
+            while True:
+                letters_set = string.ascii_letters
+                num = random.randrange(1, 10)
+                random_list = random.sample(letters_set, num)
+                random_str = f"L{''.join(random_list)}"
+                try:
+                    Prompt_log.objects.get(prompt_log_id=random_str)
+                except Prompt_log.DoesNotExist:
+                    plid = random_str
+                    break
+
+            Prompt_log.objects.create(prompt_log_id=plid, user_id=user_pk, prompt=prompt, negative_prompt=negative_prompt)
+        else: #이미 같은 내용을 검색한 기록이 있으면 시간을 변경
+            prompt_log_check[0].created_at = timezone.now()
+            prompt_log_check[0].save()
     
+    # 검색 내용을 session에 저장
+    request.session['search_query'] = {'prompt': prompt, 'negative_prompt': negative_prompt}
+    
+    # Build the URL for redirecting with pagination
+    redirect_url = reverse('search:result')
+    redirect_url += f'?page=1'
+    
+    # Redirect to ResultView with pagination
+    return redirect(redirect_url)
+
+#user가 가지고 있는 모든 prompt_log를 찾아서 제거
 def delete_all(request):
     user_pk=request.user.pk
     log_list = Prompt_log.objects.filter(user_id=user_pk)
@@ -124,6 +95,7 @@ def delete_all(request):
         log.delete()
     return redirect('search:home')
 
+#해당 prompt_log를 삭제
 def delete(request, pk):
     user_pk=request.user.pk
     log_list = Prompt_log.objects.filter(user_id=user_pk,prompt_log_id=pk)
@@ -133,8 +105,11 @@ def delete(request, pk):
 
 def tokenizer(prompt,negative_prompt):
     #positive
+
+    #<>를 제외한 모든 괄호를 제거하고 ','를 기준으로 분리
     prompt=re.sub(r'[()\[\]{}]',',',prompt)
     tok = prompt.lower().split(',')
+
     temp_prompt = ""
     for tk in tok:
         tk=make_tokenizer(tk)
@@ -142,24 +117,29 @@ def tokenizer(prompt,negative_prompt):
             continue
         if "<" in tk or ">" in tk :
             continue
+        #prompt가 존재할 경우 가중치 증가, 없을 경우 생성 후 가중치 증가
         prompt = Prompt.objects.filter(prompt=tk)
         if not prompt:
-            prompt=Prompt()
-            prompt.prompt=tk
-            prompt.positive_weight=1
-            prompt.save()
+            prompt_temp=Prompt()
+            prompt_temp.prompt=tk
+            prompt_temp.positive_weight=0
         else:
             prompt_temp = prompt[0]
-            prompt_temp.positive_weight=prompt_temp.positive_weight+1
-            prompt_temp.save()
+        prompt_temp.positive_weight=prompt_temp.positive_weight+1
+        prompt_temp.save()
+
+        #prompt를 정제하여 저장
         if temp_prompt:
             temp_prompt=temp_prompt+","+tk
         else :
             temp_prompt=tk
 
     #negative
+
+    #<>를 제외한 모든 괄호를 제거하고 ','를 기준으로 분리
     negative_prompt = re.sub(r'[()\[\]{}]',',',negative_prompt)
     tok = negative_prompt.lower().split(',')
+
     temp_negative_prompt = ""
     for tk in tok:
         tk=make_tokenizer(tk)
@@ -167,16 +147,18 @@ def tokenizer(prompt,negative_prompt):
             continue
         if "<" in tk or ">" in tk :
             continue
+        #prompt가 존재할 경우 가중치 증가, 없을 경우 생성 후 가중치 증가
         prompt = Prompt.objects.filter(prompt=tk)
         if not prompt:
-            prompt=Prompt()
-            prompt.prompt=tk
-            prompt.negative_weight=1
-            prompt.save()
+            prompt_temp=Prompt()
+            prompt_temp.prompt=tk
+            prompt_temp.negative_weight=0
         else:
             prompt_temp = prompt[0]
-            prompt_temp.negative_weight=prompt_temp.negative_weight+1
-            prompt_temp.save()
+        prompt_temp.negative_weight=prompt_temp.negative_weight+1
+        prompt_temp.save()
+
+        #prompt를 정제하여 저장
         if temp_negative_prompt:
             temp_negative_prompt=temp_negative_prompt+","+tk
         else :
@@ -210,6 +192,8 @@ def make_ai(request):
 
     return render(request,"search/rank.html",{"prompt_list":prompt_list,"negative_list":negative_prompt_list})
 '''
+
+#prompt 전체를 비교하여 가장 빈도수가 높은 데이터를 보여줌
 def rank(request):
     rank = QueryRank()
     prompt_list = rank.index_data_to_elasticsearch("prompt")
