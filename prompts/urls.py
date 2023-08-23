e20c52eade67c99bbdabb38113b0c0f688f8e6b0
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2023-08-23 15:16:48 +0900

    bookmark

--- prompts/urls.py
+++ prompts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('list/<str:positive>',PromptListView.as_view(), name='list'),
     path('detail/<str:pk>',PromptDetailView.as_view(), name='detail'),
     path('bookmark/', PromptBookmarkView.as_view(), name='bookmark'),
+    path('bookmarked/<str:positive>', BookmarkedPromptListView.as_view(), name='bookmarked'),
 ]
