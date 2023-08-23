31e5c7499f51bfa6e2458d9e04f3993c7792690a
dnjsrl132 <93039668+dnjsrl132@users.noreply.github.com>
2023-08-23 11:51:36 +0900

    temple

--- prompts/urls.py
+++ prompts/urls.py
@@ -5,6 +5,7 @@ from prompts.views import *
 app_name ="prompts"
 
 urlpatterns = [
-    path('list/',PromptListView.as_view(), name='list'),
+    path('list/<str:positive>',PromptListView.as_view(), name='list'),
     path('detail/<str:pk>',PromptDetailView.as_view(), name='detail'),
+    path('bookmark/', PromptBookmarkView.as_view(), name='bookmark'),
 ]
